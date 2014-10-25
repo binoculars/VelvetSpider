acf5020da8bd7a372ce03fcfa1d7f2244b20812f
Barrett K. Harber <barrett.harber@gmail.com>
2014-10-25 03:10:09 -0400

    default port for mongo

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url' : 'your_database_here' // looks like mongodb://<user>:<pass>@127.0.0.1:28017/velvetspider
+	'url' : 'your_database_here' // looks like mongodb://<user>:<pass>@127.0.0.1:27017/velvetspider
 };
