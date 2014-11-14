a289a4d4618323bf37bd29341510dac85a77e660
Barrett K. Harber <barrett.harber@gmail.com>
2014-11-14 02:06:57 -0500

    unused code cleanup, added TODOs

--- routes/local-auth.js
+++ routes/local-auth.js
@@ -1,29 +1,9 @@
 module.exports = function(app, passport) {
-
-// normal routes ===============================================================
-
-	// show the home page (will also have our login links)
-// 	app.get('/', function(req, res) {
-// 		res.render('index.ejs');
-// 	});
-
-	// PROFILE SECTION =========================
-	// app.get('/profile2', isLoggedIn, function(req, res) {
-	// 	res.render('profile.ejs', {
-	// 		user : req.user
-	// 	});
-	// });
-	
-	// app.get('/profile', isLoggedIn, function(req, res) {
-	// 	res.render('connect_account.ejs', {
-	// 		title: 'VelvetSpider',
-	// 		user : req.user
-	// 	});
-	// });
-	
 	app.get('/user', isLoggedIn, function(req, res) {
 		res.json(req.user);
 	});
+
+	// TODO: Make these an AJAX call
 
 	// LOGOUT ==============================
 	app.get('/logout', function(req, res) {
@@ -31,74 +11,19 @@ module.exports = function(app, passport) {
 		res.redirect('/');
 	});
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+	// process the login form
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
 
-	// locally --------------------------------
-		// LOGIN ===============================
-		// show the login form
-		//app.get('/login', function(req, res) {
-		//	res.render('login.ejs', { message: req.flash('loginMessage') });
-		//});
-
-		// process the login form
-		app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
-
-		// SIGNUP =================================
-		// show the signup form
-		//app.get('/signup', function(req, res) {
-		//	res.render('signup.ejs', { message: req.flash('loginMessage') });
-		//});
-
-		// process the signup form
-		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/signup', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
-
-// =============================================================================
-// AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
-// =============================================================================
-
-// 	// locally --------------------------------
-// 		app.get('/connect/local', function(req, res) {
-// 			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-// 		});
-// 		app.post('/connect/local', passport.authenticate('local-signup', {
-// 			successRedirect : '/profile', // redirect to the secure profile section
-// 			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-// 			failureFlash : true // allow flash messages
-// 		}));
-
-
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-// 	// local -----------------------------------
-// 	app.get('/unlink/local', function(req, res) {
-// 		var user            = req.user;
-// 		user.local.email    = undefined;
-// 		user.local.password = undefined;
-// 		user.save(function(err) {
-// 			res.redirect('/profile');
-// 		});
-// 	});
-
-	//app.get('/query_console', isLoggedIn, function(req, res) {
-	//	res.render('query_console.ejs', {
-	//		title : 'VelvetSpider'
-	//	});
-	//});
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
 };
 
 // route middleware to ensure user is logged in
