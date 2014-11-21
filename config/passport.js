d68cf6dc9fd5cbb47359be4ae337fe463f3d4a85
Barrett Harber <barrett.harber@gmail.com>
2014-11-21 16:04:29 +0000

    DRYer code for passport config

--- config/passport.js
+++ config/passport.js
@@ -70,52 +70,27 @@ module.exports = function(app, passport) {
         passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
     },
     function(req, username, password, done) {
-
-        // asynchronous
-        process.nextTick(function() {
-
+        process.nextTick(function() { // asynchronous
             //  Whether we're signing up or connecting an account, we'll need
             //  to know if the username address is in use.
             User.findOne({'local.username': username}, function(err, existingUser) {
-
-                // if there are any errors, return the error
-                if (err)
+                if (err) // if there are any errors, return the error
                     return done(err);
-
-                // check to see if there's already a user with that username
-                if (existingUser) 
+                
+                if (existingUser) // check to see if there's already a user with that username
                     return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
 
-                //  If we're logged in, we're connecting a new local account.
-                if(req.user) {
-                    var user            = req.user;
-                    user.local.username    = username;
-                    user.local.password = user.generateHash(password);
-                    user.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, user);
-                    });
-                } 
-                //  We're not logged in, so we're creating a brand new user.
-                else {
-                    // create the user
-                    var newUser            = new User();
-
-                    newUser.local.username    = username;
-                    newUser.local.password = newUser.generateHash(password);
-
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-
-                        return done(null, newUser);
-                    });
-                }
-
+                var user = req.user ? req.user : new User();
+                
+                user.local.username = username;
+                user.local.password = user.generateHash(password);
+                user.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, user);
+                });
             });
         });
-
     }));
 
     require('../models/services').authLibs.forEach(function(service) {
